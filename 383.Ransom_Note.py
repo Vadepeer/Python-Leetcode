910565853398f1506258582b7ff72c1cd3228d32
Vadepeer04 <102405641+Vadepeer@users.noreply.github.com>
2023-10-18 12:04:24 +0530

    Add files via upload

---
+++ 383.Ransom_Note.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
+        for i in ransomNote:
+            if i in magazine: magazine.remove(i)
+            else: return False
+        return True
